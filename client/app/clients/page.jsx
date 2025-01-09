30c41008795822f10d1acbe3d8dcae1da4d43b3c
Anekantjainsagar <anekantjainsagar@gmail.com>
2025-01-09 12:15:20 +0530

    Updated

--- client/app/clients/page.jsx
+++ client/app/clients/page.jsx
@@ -28,7 +28,7 @@ const Overview = () => {
       <AddAgency showSubscribe={addAgency} setShowSubscribe={setAddAgency} />
       <div className="w-[85%] bg-main h-full relative">
         <div className="bg-newBubbleColor/10 w-[50vw] h-[30vh] absolute top-1/2 -translate-y-1/2 rounded-full"></div>
-        <div className="bg-newBubbleColor/10 w-[20vw] h-[20vw] right-0 absolute top-3/6 rounded-full"></div>
+        <div className="bg-newBubbleColor/10 w-[20vw] h-[20vw] right-0 absolute top-3 /6 rounded-full"></div>
         <div className="bg-newBubbleColor/10 w-[20vw] h-[20vw] right-20 absolute bottom-10 rounded-full"></div>
         <div className="absolute backdrop-blur-3xl top-0 left-0 w-full h-full px-5 overflow-y-auto">
           <Navbar />
